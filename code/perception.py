7c101612a8821a044d740b7ccb5dcaa10106cbb7
tom <ssisksl77@gmail.com>
2017-08-27 03:55:47 +0900

    complete code and explain in README.md

--- code/perception.py
+++ code/perception.py
@@ -1,37 +1,39 @@
 import numpy as np
 import cv2
 
+
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
 def color_thresh(img, rgb_thresh=(160, 160, 160)):
     # Create an array of zeros same xy size as img, but single channel
-    color_select = np.zeros_like(img[:,:,0])
+    color_select = np.zeros_like(img[:, :, 0])
     # Require that each pixel be above all three threshold values in RGB
     # above_thresh will now contain a boolean array with "True"
     # where threshold was met
-    above_thresh = (img[:,:,0] > rgb_thresh[0]) \
-                & (img[:,:,1] > rgb_thresh[1]) \
-                & (img[:,:,2] > rgb_thresh[2])
+    above_thresh = (img[:, :, 0] > rgb_thresh[0]) \
+                & (img[:, :, 1] > rgb_thresh[1]) \
+                & (img[:, :, 2] > rgb_thresh[2])
     # Index the array of zeros with the boolean array and set to 1
     color_select[above_thresh] = 1
     # Return the binary image
     return color_select
+
 
 # Define a function to convert from image coords to rover coords
 def rover_coords(binary_img):
     # Identify nonzero pixels
     ypos, xpos = binary_img.nonzero()
     # Calculate pixel positions with reference to the rover position being at the 
-    # center bottom of the image.  
+    # center bottom of the image.
     x_pixel = -(ypos - binary_img.shape[0]).astype(np.float)
-    y_pixel = -(xpos - binary_img.shape[1]/2 ).astype(np.float)
+    y_pixel = -(xpos - binary_img.shape[1]/2).astype(np.float)
     return x_pixel, y_pixel
 
 
 # Define a function to convert to radial coords in rover space
 # 극좌표계로 변환해서 현재바라보고 있는 곳의 각도를 알아낸다. (아크탄젠트를 쓰는이유)
 def to_polar_coords(x_pixel, y_pixel):
-    # Convert (x_pixel, y_pixel) to (distance, angle) 
+    # Convert (x_pixel, y_pixel) to (distance, angle)
     # in polar coordinates in rover space
     # Calculate distance to each pixel
     dist = np.sqrt(x_pixel**2 + y_pixel**2)
@@ -39,21 +41,22 @@ def to_polar_coords(x_pixel, y_pixel):
     angles = np.arctan2(y_pixel, x_pixel)
     return dist, angles
 
+
 # Define a function to map rover space pixels to world space
 def rotate_pix(xpix, ypix, yaw):
     # Convert yaw to radians
     yaw_rad = yaw * np.pi / 180
     xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
-                            
     ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
-    # Return the result  
+    # Return the result
     return xpix_rotated, ypix_rotated
 
-def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
+
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale):
     # Apply a scaling and a translation
     xpix_translated = (xpix_rot / scale) + xpos
     ypix_translated = (ypix_rot / scale) + ypos
-    # Return the result  
+    # Return the result
     return xpix_translated, ypix_translated
 
 
@@ -77,22 +80,22 @@ def perspect_transform(img, src, dst):
     # keep same size as input image
     warped = cv2.warpPerspective(img, M,
                                  (img.shape[1], img.shape[0]))
-    mask = cv2.warpPerspective(np.ones_like(img[:, :, 0]),
-                               M, (img.shape[1], img.shape[0]))
-    return warped, mask
+
+    return warped
 
 
-def find_rocks(img, levels=(110, 110, 60)):
-    rockpix = ((img[:, :, 0] > levels[0])
-               & (img[:, :, 1] > levels[1])
-               & (img[:, :, 2] < levels[2]))
-    color_selected = np.zeros_like(img[:, :, 0])
-    color_selected[rockpix] = 1
-    return color_selected
+def find_rocks(img):
+    # find rock by using RGB value
+    rockpixels = ((img[:, :, 0] > 100)
+                  & (img[:, :, 1] > 100)
+                  & (img[:, :, 2] < 50))
+    # Convert TRUE -> 1
+    return rockpixels.astype(int)
 
 
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
+    # USE examples what I've learned in previous lecture!
     dst_size = 5
     bottom_offset = 6
     image = Rover.img
@@ -104,14 +107,15 @@ def perception_step(Rover):
          [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
          ])
     # perspect_transform으로 좌표를 변환한다. (위에서 보는 걸로 바꾸고)
-    warped, mask = perspect_transform(Rover.img, source, destination)
+    warped = perspect_transform(image, source, destination)
+
     # 흑백화를 한다.
     threshed = color_thresh(warped)
     # 장애물을 처리한다.
-    obs_map = np.absolute(np.float32(threshed) - 1) * mask
+    obs_map = np.around(np.absolute(np.float32(threshed)-1))
 
-    Rover.vision_image[:,:,2] = threshed * 255
-    Rover.vision_image[:,:,0] = obs_map * 255
+    Rover.vision_image[:, :, 2] = threshed * 255
+    Rover.vision_image[:, :, 0] = obs_map * 255
 
     # 여기에 월드맵에 좌표계를 맞춰보자.
     xpix, ypix = rover_coords(threshed)
@@ -124,6 +128,7 @@ def perception_step(Rover):
     obs_x_world, obs_y_world = pix_to_world(obsxpix, obsypix,
                                             Rover.pos[0], Rover.pos[1],
                                             Rover.yaw, world_size, scale)
+
     Rover.worldmap[y_world, x_world, 2] += 10
     Rover.worldmap[obs_y_world, obs_x_world, 0] += 1
 
@@ -136,14 +141,13 @@ def perception_step(Rover):
     Rover.nav_angles = angles
 
     # 바위 찾아내기
-    rock_map = find_rocks(warped, levels=(110, 110, 50))
+    rock_map = find_rocks(warped)
     if rock_map.any():
         rock_x, rock_y = rover_coords(rock_map)
-
         rock_x_world, rock_y_world = pix_to_world(rock_x, rock_y, Rover.pos[0],
                                                   Rover.pos[1], Rover.yaw, world_size, scale)
         rock_dist, rock_ang = to_polar_coords(rock_x, rock_y)
-        rock_idx = np.argmin(rock_dist)
+        rock_idx = int((np.argmin(rock_dist) + np.argmax(rock_dist)) / 2)
         rock_xcen = rock_x_world[rock_idx]
         rock_ycen = rock_y_world[rock_idx]
 
